d35de963e6c37d8f2306f6bcb10dfd60ab8b36c2
alexs <a.serebryansky@isida.by>
2021-08-26 14:05:02 +0300

    Swing

---
+++ Main/src/Swing.java
@@ -0,0 +1,8 @@
+import javax.print.attribute.standard.JobMessageFromOperator;
+import javax.swing.*;
+
+public class Swing {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello, world!");
+    }
+}
