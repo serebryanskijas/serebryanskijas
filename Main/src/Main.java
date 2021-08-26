3f96493dcd169754cccaa1b69a0600f770d05324
alexs <a.serebryansky@isida.by>
2021-08-26 11:40:35 +0300

    second commit

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world");
+        System.out.println("Привет, МИР");
+        System.out.println("Еще раз привет");
+        System.out.println("Commit");
     }
 }
