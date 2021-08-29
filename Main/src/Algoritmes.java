290e9de47deb7262fb74d5af754b35f3878245b0
azranar-66 <vorgen95@gmail.com>
2021-08-29 18:48:39 +0300

    add a few classes

---
+++ Main/src/Algoritmes.java
@@ -0,0 +1,8 @@
+public class Algoritmes {
+    public static void main(String[] args) {
+//linial algoritme
+        System.out.println("Wake up, Neo");
+        System.out.println("Get up!");
+        System.out.println("Make the bed");
+    }
+}
